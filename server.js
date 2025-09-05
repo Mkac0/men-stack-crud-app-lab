cced09c2d907f94916cf0fee303d79f36cfda548
melabreu <melissa.abreu84@gmail.com>
2025-09-05 14:41:10 -0400

    added blog route

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT
 
+const Blog = require('./models/blog.js');
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -12,15 +13,25 @@ db.on('error', (err) => {console.log('ERROR: ', err)});
 db.on('connected', () => {console.log(`Connected to MongoDB ${mongoose.connection.name}.`)});
 db.on('disconnected', () => {console.log('mongo disconnected')});
 
-const Blog = require('./models/blog.js');
+app.use(express.urlencoded({ extended: false }));
 
 // Route
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/blog", async (req, res) => {
+  const allBlogs = await Blog.find();
+  res.render("blog/index.ejs", { blog: allBlogs});
+});
+
 app.get("/blog/new", (req, res) => {
     res.render("blog/new.ejs");
+});
+
+app.post("/blog", async (req, res) => {
+  console.log(req.body);
+  res.redirect("/blog/new");
 });
 
 // Listener

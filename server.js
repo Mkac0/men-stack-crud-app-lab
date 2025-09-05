d676ec0527bea267e0157f3b6fd41e12b887eea9
melabreu <melissa.abreu84@gmail.com>
2025-09-05 16:34:25 -0400

    route debugged lol

--- server.js
+++ server.js
@@ -4,18 +4,25 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT
 
-const Blog = require('./models/blog.js');
+const path = require("path");
 const mongoose = require('mongoose');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
+// database
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection
 db.on('error', (err) => {console.log('ERROR: ', err)});
 db.on('connected', () => {console.log(`Connected to MongoDB ${mongoose.connection.name}.`)});
 db.on('disconnected', () => {console.log('mongo disconnected')});
 
+// Models
+const Blog = require('./models/blog.js');
+
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 
-// Route
+// Routes
 app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
@@ -29,9 +36,20 @@ app.get("/blog/new", (req, res) => {
     res.render("blog/new.ejs");
 });
 
+app.get("/blog/:blogId", async (req, res) => {
+  const foundBlog = await Blog.findById(req.params.blogId);
+  res.render("blog/show.ejs", { blog: foundBlog });
+});
+
 app.post("/blog", async (req, res) => {
-  console.log(req.body);
-  res.redirect("/blog/new");
+  try {
+    await Blog.create(req.body);
+    res.redirect("/blog/new");
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Failed to create blog");
+  }
+  res.redirect("/blog");
 });
 
 // Listener
